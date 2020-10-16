d9855c852a4f99260885705ae413ad144d8ad256
matianning <60898311+matianning@users.noreply.github.com>
2020-10-16 16:43:55 +0200

    Fin TP-AR

---
+++ TP4/Assets/animObj.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animObj : MonoBehaviour
+{
+    private Animation anim;
+    public string animName;
+    public ParticleSystem ps;
+   
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+        ps = GetComponent<ParticleSystem>();
+        if (ps != null) ps.Stop();
+
+    }
+
+    void Update()
+    {
+    }
+
+    public void animate()
+    {
+        if(anim!=null) anim.Play(animName);
+        if (ps != null) ps.Play();
+    }
+}
